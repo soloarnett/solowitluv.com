746da86520d7682a122784aed29f287d5b17cf31
Solo <solomonarnett@gmail.com>
2026-01-14 21:34:43 -0500

    added pwa compatibility

--- site/src/main.ts
+++ site/src/main.ts
@@ -4,16 +4,19 @@ import { AppComponent } from './app/app.component';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { routes } from './app/app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideServiceWorker } from '@angular/service-worker';
+import { isDevMode } from '@angular/core';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(
-      routes,
-      withInMemoryScrolling({
-        scrollPositionRestoration: 'top',  // <— always scroll to top
-        anchorScrolling: 'enabled'         // <— supports #hash links
-      })
-    ),
-    provideHttpClient()
-  ]
+    provideRouter(routes, withInMemoryScrolling({
+        scrollPositionRestoration: 'top', // <— always scroll to top
+        anchorScrolling: 'enabled' // <— supports #hash links
+    })),
+    provideHttpClient(),
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+]
 });
